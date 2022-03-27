7cdb45aee2913db4f6d61174f6d4b81a1ecd40d0
sebek78 <sebasporek@gmail.com>
2022-03-27 10:44:41 +0200

    Add React Query to app and add environment variables

--- apps/running-stats/src/app/app.tsx
+++ apps/running-stats/src/app/app.tsx
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 import { Route, Link } from 'react-router-dom';
 
 const StyledApp = styled.div`
-  // Your style here
+    // Your style here
 `;
 
 export function App() {
-  return <StyledApp>Running Stats</StyledApp>;
+    console.log(process.env['NX_RS_CLIENT_ID']);
+    return <StyledApp>Running Stats</StyledApp>;
 }
 
 export default App;
--- apps/running-stats/src/main.tsx
+++ apps/running-stats/src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './app/app';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-  document.getElementById('root')
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </QueryClientProvider>
+    </StrictMode>,
+    document.getElementById('root')
 );
